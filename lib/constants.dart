fc0d558f4c4faef80b16adac5f44e94eefaafa19
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-02-15 12:57:32 -0800

    Update min/max constants for diastolic and systolic data

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,9 +6,9 @@ class Constants {
   static const String COLUMN_TIMESTAMP = 'timestamp';
   static const String DB_PATH = 'bpdata.db';
 
-  static const int DIASTOLIC_MAX = 150;
-  static const int DIASTOLIC_MIN = 60;
+  static const int DIASTOLIC_MAX = 110;
+  static const int DIASTOLIC_MIN = 50;
 
-  static const int SYSTOLIC_MAX = 120;
-  static const int SYSTOLIC_MIN = 60;
+  static const int SYSTOLIC_MAX = 150;
+  static const int SYSTOLIC_MIN = 50;
 }
