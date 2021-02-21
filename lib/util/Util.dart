6dbddf2a3746d4fc601a1dd4d0e7001e0d657c31
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-02-21 14:01:05 -0800

    Add utility method to show snackbar

--- lib/util/Util.dart
+++ lib/util/Util.dart
@@ -1,6 +1,22 @@
 import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
 
 class Util {
   static String formatTime(DateTime time, {String locale: ''}) =>
       "${new DateFormat.yMd().format(time)} at ${new DateFormat.jms().format(time)}";
+
+  static void showSnackBar(
+    BuildContext context,
+    String message, {
+    int duration: 1500,
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(milliseconds: duration),
+        content: Text(
+          message,
+        ),
+      ),
+    );
+  }
 }
